db00d0dc041efd299b4b00772d4e8cf5194efca0
ann-wangui <wanguia822@gmail.com>
2022-05-19 06:56:51 +0300

    add all changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,43 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask 
+from config import Config
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_uploads import IMAGES, UploadSet,configure_uploads
+from config import config_options
 
 db = SQLAlchemy()
+mail = Mail()
+bootstrap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+
+
+
 def create_app(config_name):
     app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    # app.config.from_object(Config)
+    from .auth import auth as authentication_blueprint
+    from .main import main as main_blueprint
+
+    app.register_blueprint(authentication_blueprint)
+    app.register_blueprint(main_blueprint)
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    bootstrap.init_app(app)
+    # configure_uploads(app,photos)
+    mail.init_app(app)
+
+    # @app.shell_context_processor
+    # def make_shell_context():
+    #  return dict(db=db, User=User )
+
+    return app
