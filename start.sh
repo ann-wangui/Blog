0aed748b01bd35d2fafbe8799ace6d42fe888b14
ann-wangui <wanguia822@gmail.com>
2022-05-19 06:53:11 +0300

    add a mail username

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
-SECRET_KEY = '83aea91d0a6d350aa3c6c22e138493f0ca5792de'
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://anne:wangui22@localhost/blogs' 
+export SECRET_KEY = '83aea91d0a6d350aa3c6c22e138493f0ca5792de'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://anne:wangui22@localhost/blogs' 
+export MAIL_USERNAME="ann.wangui@student.moringaschool.com"
+
+python3 manage.py server
